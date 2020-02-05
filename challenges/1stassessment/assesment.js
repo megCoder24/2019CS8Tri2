1d7581ffe10c53fadb0987f5e5eb5445b8bd42b8
megCoder24 <mpfannerstill24@nscds.org>
2020-02-05 17:14:17 +0000

    ASSESMENT1

---
+++ challenges/1stassessment/assesment.js
@@ -0,0 +1,10 @@
+$(".thing").click(
+    function(){
+       alert("hello")
+    }
+);
+$(".thing type1").click(
+    function(){
+       var clickedThing = $(this).css("background-image");
+    }
+);
